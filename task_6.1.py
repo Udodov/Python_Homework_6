2e5cf20af99b57dd747205522dc15557dcdd2d04
«Udodov» <k.udodov@internet.ru>
2022-08-12 11:43:36 +0300

    In the file 'task_6.1.py' changes have been made, modification of the 'List Comprehension' is completed

--- task_6.1.py
+++ task_6.1.py
@@ -7,18 +7,13 @@ def read_from_file(path_file: str) -> str:
         return file.readline()
 
 
-def del_some_words(text: str) -> str:
-    text = []
-    for item in read_text:
-        if deleted_word not in item:
-            text.append(item)
-    return " ".join(text)
-
-
 path = os.path.join('documentation6', 'task_6.1.txt')
 
 read_text = read_from_file(path).split()
 print(f'There is text in the file:\n ', " ".join(read_text))
 deleted_word = input('Enter the word you want to remove from the text as a string:')
+
+del_some_words = [item for item in read_text if deleted_word not in item]
+
 print()
-print(f'The final text looks like this:\n ', del_some_words(read_from_file(path)))
+print(f'The final text looks like this:\n ', " ".join(del_some_words))
