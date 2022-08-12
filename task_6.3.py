d9878b08ab3410429da86f385fa6f5993ed1d38f
«Udodov» <k.udodov@internet.ru>
2022-08-12 15:27:04 +0300

    In the file 'task_6.3.py' changes have been made, modification of the 'List Comprehension' is completed

--- task_6.3.py
+++ task_6.3.py
@@ -1,12 +1,9 @@
-def set_product_numbers(number_N: int):
-    multiplier = [1]
-    for i in range(1, number_N):
-        multiplier.append(multiplier[i - 1] * (i + 1))
-    return multiplier
-
-
 number_N = int(input('Enter the number "N" to create a multiplication set: '))
 if number_N < 0:
     number_N *= -1
+
+multiplier = [1]
+set_product_numbers = [i for i in range(1, number_N) if multiplier.append(multiplier[i - 1] * (i + 1))]
+
 print(f'Multiplication of numbers, from 1 to a number '
-      f'{number_N} looks like this:', set_product_numbers(number_N))
+      f'{number_N} looks like this:', multiplier)
